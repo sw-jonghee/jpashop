63abf01d04264547ab99a3196112ccd55615880c
jonghee <sw.jonghee@gmail.com>
2021-09-09 10:33:43 +0900

    다양한 연관관계 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    private Long Id;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+}
